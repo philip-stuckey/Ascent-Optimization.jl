51be9437e09c810b287b7356cb59bd87baf67c71
Philip Stuckey <developer.philip_stuckey@fastmail.com>
2022-11-02 10:58:30 -0400

    exporting Vec and basis

--- src/OrbitalMechanics.jl
+++ src/OrbitalMechanics.jl
@@ -3,6 +3,8 @@ module OrbitalMechanics
 using StaticArrays
 using LinearAlgebra
 
+export Vec, basis
+
 const Vec = SVector{3, Float64}
 basis(r⃗) = (normalize(r⃗), [0 1 0; -1 0 0; 0 0 0]*normalize(r⃗) )
 
