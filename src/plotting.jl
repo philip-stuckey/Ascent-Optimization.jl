d1c5c7fbf9d3187c549a61ff60567b55217cd651
Philip Stuckey <developer.philip_stuckey@fastmail.com>
2022-11-06 18:35:19 -0500

    added animation function

--- src/plotting.jl
+++ src/plotting.jl
@@ -1,6 +1,7 @@
 
 using Plots
 using Printf
+using GeometryBasics: Point2
 
 colTuple(v::Vector) = tuple(([u] for u in v)...)
 function plot_orbit(ship, body; legend=false, aspect_ratio=1, kwargs...)
@@ -24,4 +25,28 @@ function plot_orbit(ship, body; legend=false, aspect_ratio=1, kwargs...)
 		(periapsis_point...,"$(@sprintf("%.2f",periapsis(ship,body)))")
 	])
 	return plt
+end
+
+function animate_path(path, body)
+	anim = @animate for (n,ship) in enumerate(path)
+		plt1 = plot_orbit(ship,body)
+		plot!(Point2.(ship.position[1:2] for ship in path[1:n]))
+		quiver!(Point2(ship.position[1:2]), quiver=colTuple(100*heading(ship)[1:2]))
+		e =eccentricity_vector(ship,body)*100
+		quiver!([0],[0], quiver=([e[1], [e[2]]]))
+		
+		plt2 = plot(Point2.(ship.velocity[1:2] for ship in path[1:n]))
+		scatter!(Point2(path[n].velocity[1:2]))
+
+		plt3 = plot(Point2.(thrust_vector(ship)[1:2] for ship in path[1:n]))
+		scatter!(Point2(thrust_vector(path[n])[1:2]))
+
+		plot(
+			plt1,
+			#plt2, 
+			#plt3
+		)
+	end 
+	mov(anim)
+
 end
