06e7507ba2d15298b4adc0f1fe3b336df681aea2
Philip Stuckey <developer.philip_stuckey@fastmail.com>
2022-11-08 19:49:16 -0500

    changed sign of eccentiricity vector

--- src/plotting.jl
+++ src/plotting.jl
@@ -6,7 +6,7 @@ using GeometryBasics: Point2
 colTuple(v::Vector) = tuple(([u] for u in v)...)
 function plot_orbit(ship, body; legend=false, aspect_ratio=1, kwargs...)
 	
-	ê = normalize(eccentricity_vector(ship,body))
+	ê = normalize(-eccentricity_vector(ship,body))
 	ϕ = acos(ê[1:2]⋅[1,0])
 	θ = range(0,2π, length=100)
 	R = body.radius
@@ -32,7 +32,7 @@ function animate_path(path, body)
 		plt1 = plot_orbit(ship,body)
 		plot!(Point2.(ship.position[1:2] for ship in path[1:n]))
 		quiver!(Point2(ship.position[1:2]), quiver=colTuple(100*heading(ship)[1:2]))
-		e =eccentricity_vector(ship,body)*100
+		e = eccentricity_vector(ship,body)*100
 		quiver!([0],[0], quiver=([e[1], [e[2]]]))
 		
 		plt2 = plot(Point2.(ship.velocity[1:2] for ship in path[1:n]))
