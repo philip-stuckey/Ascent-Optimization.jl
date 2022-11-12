42041143c123d77ed689a0ccd33a507405686bad
Philip Stuckey <developer.philip_stuckey@fastmail.com>
2022-11-12 11:20:22 -0500

    moved some gradient descent methods from notebook to file

--- src/MachineLearning/gradient_descent.jl
+++ src/MachineLearning/gradient_descent.jl
@@ -1,10 +1,36 @@
 using FiniteDiff: finite_difference_gradient
 
-function loss(θ) 
-    model = Model(θ[1], θ[2:end])
-    rₘₐₓ = delta_v(standard_parameters.initial_ship)
-    (ship, _) = runModel(model, standard_parameters, path=nothing)
-    return (rₘₐₓ - delta_v(ship))/float(periapsis(ship,body) > standard_parameters.target_altitude)
+function reward(θ)
+	body = standard_parameters.body
+	target_altitude = standard_parameters.target_altitude
+	
+	model = Model(θ[1], θ[2:end])
+	(ship, _) = runModel(model, standard_parameters, path=nothing)
+	return delta_v(ship)*(target_altitude < periapsis(ship,body))
+end
+
+∇reward(θ) = finite_difference_gradient(reward, θ, relstep=0.01)
+
+function trainModel!(∇reward; θ₀=zeros(3), steps=28, α=0.001, min_marginal_reward=0.0)
+	θ = θ₀[:]
+	points = zeros(steps,length(θ))
+	grads = zeros(steps,length(θ))
+	rewards = zeros(steps) .- 1
+
+	for n in 1:steps
+		grad =  ∇reward(θ)
+		θ .+= α .* grad
+		r = reward(θ)
+		if r - get(rewards,n-1, 0) < min_marginal_reward
+			θ .-= α .* grad
+			@info "marginal reward too low" r-get(rewards,n-1,0) min_marginal_reward
+			break
+		end
+		rewards[n] = r
+		points[n,:] .= θ
+		grads[n,:] .= grad
+	end
+	return (model=Model(θ[1],θ[2:end]), rewards=rewards, path=points, grads=grads)
 end
 
 
