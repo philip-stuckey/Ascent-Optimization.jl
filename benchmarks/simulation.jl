a932abee94bf60385b97d968b7d0886cc00c4224
Philip Stuckey <developer.philip_stuckey@fastmail.com>
2022-11-13 08:48:27 -0500

    made benchmark unitful

--- benchmarks/simulation.jl
+++ benchmarks/simulation.jl
@@ -1,9 +1,10 @@
 using OrbitalMechanics
 using BenchmarkTools
+using Unitful: s
 
 ship = standard_parameters.initial_ship
 body = standard_parameters.body
-Δt=0.001
+Δt=0.001s
 
 @info "Simulate!" @benchmark OrbitalMechanics.Simulate!($(deepcopy(ship)), $body; Δt = $Δt)
 
