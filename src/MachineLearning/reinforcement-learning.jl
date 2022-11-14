b609740f81d784d18d8afc54aa64e2e9938d5e85
Philip Stuckey <developer.philip_stuckey@fastmail.com>
2022-11-14 12:08:28 -0500

    separated epsilon explorer into an explorer and model struct

--- src/MachineLearning/reinforcement-learning.jl
+++ src/MachineLearning/reinforcement-learning.jl
@@ -3,22 +3,14 @@ using Base.Iterators: product, flatten
 
 randargmax(A) = rand([n for (n,a) in enumerate(A) if a .== maximum(A)])
 
-struct EpsilonExplorer{A,R}
+struct EpsilonExplorer
     learning_rate::Float64
     exploration_rate::Float64
-    quality::Dict{A, R}
-    count::Dict{A, Int}
 end
 
-function EpsilonExplorer{R}(ε, α, actions) where R <: Number
-    default_quality = Dict(actions .=> zero(R))
-    default_count = Dict(actions .=> 0)
-    return EpsilonExplorer(ε, α, default_quality, default_count)
-end
-
-function choice(explorer::EpsilonExplorer)
-	actions = collect(keys(explorer.quality))
-	vals = values(explorer.quality)
+function choice(explorer::EpsilonExplorer, model::QModel)
+	actions = collect(keys(model.quality))
+	vals = values(model.quality)
 	return if explorer.learning_rate >= rand()
 		rand(actions)
 	else
@@ -26,9 +18,16 @@ function choice(explorer::EpsilonExplorer)
 	end
 end
 
-function update!(explorer::EpsilonExplorer, action, reward)
-	explorer.N[action] += 1
-	explorer.quality[action] += explorer.learning_rate * (reward - explorer.quality[action])
+struct QModel{A}
+	quality::Dict{A, Float64}
+    count::Dict{A, Int}
+end
+
+QModel{R}(actions) where R = QModel(Dict(actions.=>zero(R)), Dict(actions.=>0))
+
+function update!(model::QModel, action, reward, learning_rate)
+	model.count[action] += 1
+	model.quality[action] += learning_rate * (reward - model.quality[action])
 end
 
 
@@ -61,30 +60,25 @@ end
 function train_model!(
 	m₀::Model, 
 	simulation_parameters::SimulationParameters, 
-	params::TrainingParameters; 
+	explorer::EpsilonExplorer,
+	action_space; 
 	rewards=nothing
 )
 	r₀ = reward(m₀, simulation_parameters)
-	explorer = EpsilonExplorer{Float64}(
-		params.learning_rate, 
-		params.exploration_rate, 
-		action_space(m₀)
-	)
 	
+	learner = QModel(action_space)
 	model = m₀
 	for n in 1:params.step_limit
 		rewards == nothing || push!(rewards, r₀)
-		action = choice(explorer)
+		action = choice(explorer, learner)
 		model = apply(model, action)
 		r = reward(model, simulation_parameters)
-		update!(explorer, action, r-r₀)
+		update!(learner, action, r-r₀, explorer.learning_rate)
 		if r < r₀
 			model = apply(model, negate_action(action))
 		else
 			r₀ = r
 		end
-        		
-		r > params.max_reward && break
 	end
 	return model
 end
