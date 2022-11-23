985ff476ce828270f2bdb95f8b1ece970a5f8eb1
Philip Stuckey <pstuc002@gmail.com>
2022-11-22 20:16:51 -0500

    added celestial bodies from ksp

---
+++ src/planets.jl
@@ -0,0 +1,92 @@
+
+const Moho = Planet(
+	1.6860938e11*m^3/s^2,
+	250_000m,
+	1/(1_210_000.0s)
+)
+
+const Eve = Planet(
+	8.1717302e12m^3/s^2,
+	700_000m, 
+	1/(80_500.000s)
+)
+
+const Gilly = Planet(
+	8_289_449.8*m^3/s^2,
+	13_000m,
+	1/(28_255.000s)
+)
+
+const Kerbin = Planet(
+	3.5316000e12m^3/s^2,
+	600_000m,
+	1/(21_549.425s)
+)
+
+const Mun = Planet(
+	6.5138398e10m^3/s^2,
+	200_000m,
+	1/(138984s)
+)
+
+const Minimus = Planet(
+	1.7658000e9m^3/s^2,
+	60_000m,
+	1/(40_400.000s)
+)
+
+const Duna = Planet(
+	3.0136321e11m^3/s^2,
+	320_000m,
+	1/(65_517.859s)
+)
+
+const Ike = Planet(
+	1.8568369e10m^3/s^2,
+	130_000m,
+	1/(65_517.862s)
+)
+
+# No one remembers Dres
+
+const Jool = Planet(
+	2.8252800e14m^3/s^2,
+	6_000_000m,
+	1/(36_000s)
+)
+
+const Laythe = Planet(
+	1.9620000e12m^3/s^2,
+	500_000m,
+	1/(52_980.879s)
+)
+
+const Val = Planet(
+	2.0748150e11m^3/s^2,
+	300_000m,
+	1/(105_962.09s)
+)
+
+const Tylo = Planet(
+	2.8252800e12m^3/s^2,
+	600_000m,
+	1/(211_926.36s)
+)
+
+const Bop = Planet(
+	2.4868349e9m^3/s^2,
+	65_000m,
+	1/(544_507.43s)
+)
+
+const Pol = Planet(
+	7.2170208e8m^3/s^2,
+	44_000m,
+	1/(901_902.62s)
+)
+
+const Eeloo = Planet(
+	7.4410815e10m^3/s^2,
+	210_000m,
+	1/(19_460.000s)
+)
